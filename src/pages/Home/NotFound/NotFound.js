01f67cfa984086442aa3952d589f41b43c889524
devruhul <ruhulamin262001@gmail.com>
2022-06-30 16:45:00 +0600

    created essential components

---
+++ src/pages/Home/NotFound/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Nothing found, please go back to home</h2>
+        </div>
+    );
+};
+
+export default NotFound;
