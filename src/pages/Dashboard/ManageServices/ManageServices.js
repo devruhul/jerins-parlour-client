bd4b9f47f7dbdf033b1df7939a31448ff0ba1554
devruhul <ruhulamin262001@gmail.com>
2022-08-01 00:48:19 +0600

    added loading in services list

--- src/pages/Dashboard/ManageServices/ManageServices.js
+++ src/pages/Dashboard/ManageServices/ManageServices.js
@@ -3,12 +3,21 @@ import ManageService from './ManageService/ManageService';
 
 const ManageServices = () => {
     const [handleServices, setHandleServices] = useState([]);
-    
+
     useEffect(() => {
         fetch('http://localhost:5000/services')
             .then(res => res.json())
             .then(data => setHandleServices(data))
     }, []);
+
+    // loading
+    if (handleServices.length === 0) {
+        return (
+            <div className='text-center my-6'>
+                <i className="fa-solid fa-3x fa-spinner"></i>
+            </div>
+        )
+    }
 
     return (
         <section className="text-gray-600 body-font">
