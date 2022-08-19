cc03e20a0ba173285eed9e85c7a2dbbff5d358ff
devruhul <ruhulamin262001@gmail.com>
2022-08-19 22:06:50 +0600

    fix: comment out facebook login later add this

--- src/pages/Authentication/Login/Login.js
+++ src/pages/Authentication/Login/Login.js
@@ -37,10 +37,10 @@ const Login = () => {
                 Login To Your Account
             </div>
             <div className="flex gap-4 item-center">
-                <button type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                {/* <button type="button" className="py-2 px-4 flex justify-center items-center  bg-blue-600 hover:bg-blue-700 focus:ring-blue-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                     <i className="fa-brands fa-facebook mr-2"></i>
                     Facebook
-                </button>
+                </button> */}
                 <button onClick={handleGoogleSignin} type="button" className="py-2 px-4 flex justify-center items-center  bg-red-600 hover:bg-red-700 focus:ring-red-500 focus:ring-offset-red-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                     <i className="fa-brands fa-google mr-2"></i>
                     Google
