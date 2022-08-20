e82a9dcbee11d98125b2e05ceebcb4cf4d76a0ab
devruhul <ruhulamin262001@gmail.com>
2022-08-20 14:58:12 +0600

    feat: send contact to database

--- src/pages/Home/ContactUs/ContactUs.js
+++ src/pages/Home/ContactUs/ContactUs.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { useState } from 'react';
 
 const ContactUs = () => {
+
+    const [contact, setContact] = useState({})
+
+    const handleOnChange = (e) => {
+        const nameInputField = e.target.name
+        const valueInputField = e.target.value
+
+        setContact({
+            ...contact,
+            [nameInputField]: valueInputField
+        })
+    }
+
+    const handleContact = (e) => {
+        const newContact = { ...contact };
+        fetch('http://localhost:5000/contacts', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(newContact)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('Your message has been sent successfully');
+                    e.target.reset()
+                }
+            })
+        e.preventDefault();
+    }
+
     return (
         <div className="bg-light-tomato">
-            <form className="flex w-full max-w-sm space-y-3 mx-auto">
+            <form onSubmit={handleContact} className="flex w-full max-w-sm space-y-3 mx-auto">
                 <div className="w-full  px-5 py-10 m-auto mb-10 mt-10 rounded-lg shadow-lg dark:bg-gray-800">
                     <h1 className="title-font sm:text-3xl md:text-3xl text-3xl mb-4 font-bold text-gray-900">Let us handle your <br /> project,
                         <span className="text-span-text"> Professionally.</span>
@@ -11,27 +42,27 @@ const ContactUs = () => {
                     <div className="grid  grid-cols-2 gap-6 m-auto">
                         <div className="md:col-span-2 lg:col-span-1">
                             <div className=" relative ">
-                                <input required type="text" id="contact-form-name" className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="First Name" />
+                                <input required type="text" onChange={handleOnChange} id="contact-form-name" name='firstName' className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="First Name" />
                             </div>
                         </div>
                         <div className="md:col-span-2 lg:col-span-1">
                             <div className=" relative ">
-                                <input required type="text" id="contact-form-name" className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Last Name" />
+                                <input required type="text" onChange={handleOnChange} id="contact-form-name" name='lastName' className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Last Name" />
                             </div>
                         </div>
                         <div className="md:col-span-2 lg:col-span-2">
                             <div className=" relative ">
-                                <input required type="email" id="contact-form-name" className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Email Address" />
+                                <input required type="email" onChange={handleOnChange} id="contact-form-name" name='email' className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Email Address" />
                             </div>
                         </div>
                         <div className="md:col-span-2 lg:col-span-2">
                             <div className=" relative ">
-                                <input required type="number" id="contact-form-number" className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Your Number" />
+                                <input required type="number" onChange={handleOnChange} id="contact-form-number" name='phoneNumber' className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" placeholder="Your Number" />
                             </div>
                         </div>
                         <div className="col-span-2">
-                            <label className="text-gray-700" htmlFor="name">
-                                <textarea className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" id="message" placeholder="Your Message" name="comment" rows="5" cols="40">
+                            <label className="text-gray-700" onChange={handleOnChange} htmlFor="name">
+                                <textarea required className="flex-1 appearance-none border border-pink-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-pink-500 focus:border-transparent" id="message" placeholder="Your Message" name="message" rows="5" cols="40" minLength={10}>
                                 </textarea>
                             </label>
                         </div>
